1293b6b0803b02496947cb35530f1e7ea8d662f1
Miles Wells <mileswells93@gmail.com>
2019-10-12 14:30:01 -0400

    drag n drop wip

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,40 +5,67 @@ import {Resizable} from 're-resizable';
 const INITIAL_WIDTH = 400;
 const INITIAL_HEIGHT = 300;
 
+const INITIAL_X = 0;
+const INITIAL_Y = 0;
+
 interface CustomCardProps {
   height?: string | number;
   width?: string | number;
+  x?: number;
+  y?: number;
 }
 
-type CardProps = React.PropsWithChildren<CustomCardProps>;
+export type CardProps = React.PropsWithChildren<CustomCardProps>;
+
 const createStyles = makeStyles<any, Partial<CardProps>>({
   rootCard: {
-    height: ({height}) => height,
+    height: '100%',
     margin: 'unset',
     padding: 10,
+    width: '100%'
+  },
+  resizableRoot: {
+    height: ({height}) => height,
+    position: 'absolute',
+    top: ({y}) => y,
+    left: ({x}) => x,
     width: ({width}) => width
   }
 });
 
-export const Card: React.FC<React.PropsWithChildren<{}>> = ({children}) => {
-  const [height, setHeight] = useState(INITIAL_HEIGHT);
-  const [width, setWidth] = useState(INITIAL_WIDTH);
+export const Card: React.FC<React.PropsWithChildren<CardProps>> = ({
+  children,
+  height = INITIAL_HEIGHT,
+  width = INITIAL_WIDTH,
+  x = INITIAL_X,
+  y = INITIAL_Y
+}) => {
+  const [resizableHeight, setResizableHeight] = useState(height);
+  const [resizableWidth, setResizableWidth] = useState(width);
 
-  const [cardHeight, setCardHeight] = useState(INITIAL_HEIGHT);
-  const [cardWidth, setCardWidth] = useState(INITIAL_WIDTH);
+  const [cardHeight, setCardHeight] = useState(height);
+  const [cardWidth, setCardWidth] = useState(width);
 
-  const classes = createStyles({height: cardHeight, width: cardWidth});
+  const [cardX, setCardX] = useState(x);
+  const [cardY, setCardY] = useState(y);
+
+  const classes = createStyles({
+    height: cardHeight,
+    width: cardWidth,
+    x: cardX,
+    y: cardY
+  });
 
   const resize = useCallback(
-    (delta: {height: number; width: number}) => {
-      setHeight(curHeight => curHeight + delta.height);
-      setWidth(curWidth => curWidth + delta.width);
+    delta => {
+      setResizableHeight(curHeight => curHeight + delta.height);
+      setResizableWidth(curWidth => curWidth + delta.width);
     },
-    [setWidth, setHeight]
+    [setResizableWidth, setResizableHeight]
   );
 
   const resizeCard = useCallback(
-    (delta: {height: number; width: number}) => {
+    delta => {
       setCardHeight(height + delta.height);
       setCardWidth(width + delta.width);
     },
@@ -47,12 +74,13 @@ export const Card: React.FC<React.PropsWithChildren<{}>> = ({children}) => {
 
   return (
     <Resizable
+      className={classes.resizableRoot}
       bounds='parent'
       minWidth={100}
       minHeight={100}
       size={{
-        height,
-        width
+        height: resizableHeight,
+        width: resizableWidth
       }}
       onResize={(_, __, ___, delta) => resizeCard(delta)}
       onResizeStop={(_, __, ___, delta) => resize(delta)}
